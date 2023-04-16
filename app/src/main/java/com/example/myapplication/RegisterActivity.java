93c6e50c2c34fbabe77a4c5a5ac49695f7349be0
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2023-04-16 13:26:30 +0700

    Update RegisterActivity.java

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -14,6 +16,11 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //sẽ ẩn tiêu đề
+        getSupportActionBar().hide(); // ẩn thanh tiêu đề
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //bật chế độ toàn màn hình
         setContentView(R.layout.activity_register);
 
         suggestLoginBtn = findViewById(R.id.suggestLoginBtn);
